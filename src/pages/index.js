273db4b86809b81427ad299f9ea2efd5ceaebb22
baldevpardeshi <baldevpardeshi1@gmail.com>
2024-05-17 13:26:58 +0530

    fix: remove bottom footer white space adjust the avtar for porto m device

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import Header from "@/components/header/header";
-import Footer from "@/components/footer/footer";
 import Stepper from "@/components/stepper/stepper";
 import { Scene, Persona } from "@soulmachines/smwebsdk";
 
@@ -227,11 +226,6 @@ export default function Home() {
         {/* <div className="absolute bottom-0">
           <InfoPopup />
         </div> */}
-        <Footer
-         isMicOn={isMicOn}
-         handleMicOnOff={handleMicOnOff}
-         handleStopTalking={handleStopTalking}
-        />
       </div>
     </>
   );
