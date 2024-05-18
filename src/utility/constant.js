a98076e63209bcb41fe7169843a8011429f5fce8
arvindalawa07 <109585417+arvindalawa07@users.noreply.github.com>
2024-05-18 16:50:27 +0530

    feat(Index):work on the dynamic carousel

---
+++ src/utility/constant.js
@@ -0,0 +1,36 @@
+export const BILL_DEVIATION = [
+  { url: "/images/stepOne.gif" },
+  { url: "/images/stepTwo.gif" },
+  { url: "/images/stepThree.gif" },
+  { url: "/images/stepFour.gif" },
+  { url: "/images/stepFive.gif" },
+
+  { url: "/images/stepEight.gif" },
+];
+export const INTERNET_NOT_WORKING = [
+  { url: "/images/internet-1.gif" },
+  { url: "/images/internet-2.gif" },
+  { url: "/images/internet-3.gif" },
+  { url: "/images/internet-4.gif" },
+  { url: "/images/internet-5.gif" },
+  { url: "/images/internet-6.gif" },
+  { url: "/images/internet-all-set.gif" },
+];
+export const ELIFE = [
+  { url: "/images/elife-1.gif" },
+  { url: "/images/elife-2.gif" },
+  { url: "/images/elife-3.gif" },
+  { url: "/images/elife-4.gif" },
+  { url: "/images/elife-5.gif" },
+  { url: "/images/elife-6.gif" },
+  { url: "/images/elife-all-set.gif" },
+];
+export const FAQ = [
+  { url: "/images/faq-1.png" },
+  { url: "/images/faq-2.png" },
+  { url: "/images/faq-3.png" },
+  { url: "/images/faq-4.png" },
+  { url: "/images/faq-5.png" },
+];
+// { url: "/images/stepSix.gif" },
+// { url: "/images/stepSeven.gif" },
